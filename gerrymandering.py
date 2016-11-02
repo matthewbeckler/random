e3bd8703b8e638355fbb6ac80ae5c38eb7e0e2f4
Matthew Beckler <mbeckler@xirgotech.com>
2016-11-02 15:38:52 -0500

    gerrymandering update

--- gerrymandering.py
+++ gerrymandering.py
@@ -87,24 +87,24 @@ def visit(x, y):
     if x < (width - 1):
         visit(x + 1, y)
 
-def is_district_connected(thelist):
-    """ Returns True if the list of (row, col) pairs are all edge-adjacent, False otherwise. """
+def is_district_connected(reallist, unassignedlist):
+    """ Returns True if the list of (row, col) pairs in reallist are all edge-adjacent (possibly using unassignedlist to help), False otherwise. """
     if verbose:
-        print "is_district_connected(%s)" % str(thelist)
+        print "is_district_connected(%s, %s)" % (str(reallist), str(unassignedlist))
     global rclist, visited
-    rclist = thelist
+    rclist = reallist + unassignedlist
     visited = set() # track nodes we've already visited to avoid loops
     if len(rclist) == 0:
         return True
-    assert len(rclist) > 0 and len(rclist) <= voters_per_dist
+    assert len(rclist) > 0
     x, y = rclist[0]
     visit(x, y)
-    # If we could visit all the nodes, we're good
-    return sorted(visited) == sorted(thelist)
+    # If we could visit all the nodes in reallist starting with a point in reallist, we're good
+    return set(reallist).issubset(visited)
 
 verbose = False
-assert is_district_connected([(1,1),(1,2),(2,2),(2,1)])
-assert not is_district_connected([(1,1),(1,2),(2,2),(5,1)])
+assert is_district_connected([(1,1),(1,2),(2,2),(2,1)],[])
+assert not is_district_connected([(1,1),(1,2),(2,2),(5,1)],[])
 
 
 STATE_OK = 0
@@ -113,30 +113,38 @@ STATE_OK_INCOMPLETE = 2
 def check_state(state):
     # Let's check district by district to see if it's valid
     # Build a map from district to a list of its squares
-    dict_dist_to_rc = dict(zip(range(1,num_dist+1), [[] for i in range(num_dist)]))
-    for ixdistrict in range(1, num_dist+1):
+    dict_dist_to_rc = dict(zip(range(0, num_dist+1), [[] for i in range(num_dist+1)]))
+    for ixdistrict in range(0, num_dist+1):
         for ixrow in range(height):
             for ixcol in range(width):
                 if state[ixrow][ixcol] == ixdistrict:
                     dict_dist_to_rc[ixdistrict].append( (ixrow,ixcol) )
 
-#    for i in range(1, num_dist+1):
-#        print "District %2d:" % i, dict_dist_to_rc[i]
+    if verbose:
+        for i in range(0, num_dist+1):
+            print "District %2d:" % i, dict_dist_to_rc[i]
 
     all_districts_done = True
     for i in range(1, num_dist+1):
-#        print "District %d -" % i,
+        if verbose:
+            print "District %d -" % i,
         if len(dict_dist_to_rc[i]) > voters_per_dist:
-#            print "Too many voters (%d)" % len(dict_dist_to_rc[i])
+            if verbose:
+                print "Too many voters (%d)" % len(dict_dist_to_rc[i])
             return STATE_ERROR
-        if not is_district_connected(dict_dist_to_rc[i]):
-#            print "Not connected"
+
+        # Count unassigned cells too for the connectivity test
+        if not is_district_connected(dict_dist_to_rc[i], dict_dist_to_rc[0]):
+            if verbose:
+                print "Not connected"
             return STATE_ERROR
         if len(dict_dist_to_rc[i]) < voters_per_dist:
-#            print "Not finished"
+            if verbose:
+                print "Not finished"
             all_districts_done = False
         else:
-#            print "Apparently ok"
+            if verbose:
+                print "Apparently ok"
             pass
 
     if all_districts_done:
@@ -145,19 +153,81 @@ def check_state(state):
         return STATE_OK_INCOMPLETE
 
 
+def evaluate_winners(state):
+    """ Evaluates the district assignments with the voters data.
+        Returns (dist_red, dist_blue, dist_tie). """
+    dist_red = 0
+    dist_blue = 0
+    dist_tie = 0
+    if check_state(state) == STATE_ERROR:
+        return None
+    # Build a map from district to a list of its squares
+    dict_dist_to_rc = dict(zip(range(1,num_dist+1), [[] for i in range(num_dist)]))
+    for ixdistrict in range(1, num_dist+1):
+        for ixrow in range(height):
+            for ixcol in range(width):
+                if state[ixrow][ixcol] == ixdistrict:
+                    dict_dist_to_rc[ixdistrict].append( (ixrow,ixcol) )
+    for i in range(1, num_dist+1):
+        #print "District %2d:" % i, dict_dist_to_rc[i]
+        num_red = 0
+        num_blue = 0
+        for r, c in dict_dist_to_rc[i]:
+            if voters[r][c] == 0:
+                num_red += 1
+            else:
+                num_blue += 1
+        if num_red > num_blue:
+            dist_red += 1
+        elif num_red < num_blue:
+            dist_blue += 1
+        else:
+            dist_tie += 1
+    return (dist_red, dist_blue, dist_tie)
+
+def print_status(y, x):
+    global state
+    s = ""
+    for ixrow in range(height):
+        for ixcol in range(width):
+            s += str(state[ixrow][ixcol])
+            if ixrow == y and ixcol == x:
+                return s
+        s += "|"
+    return s
+
+    
+
+# For some reason the code below can't find this solution
+#state = [[1, 1, 5, 5, 5],
+#         [1, 2, 2, 5, 4],
+#         [1, 2, 2, 5, 4],
+#         [1, 3, 2, 4, 0],
+#         [0, 0, 0, 0, 0]]
+#
+#         #[1, 3, 2, 4, 4],
+#         #[3, 3, 3, 3, 4]]
+#verbose = True
 #print_state(state)
 #print check_state(state)
+#print evaluate_winners(state)
+#print print_status(3,3)
+#sys.exit()
 
-valid_states = []
+best_red_dist = 0
+best_red_states = []
+best_blue_dist = 0
+best_blue_states = []
 
 def go(y, x):
     """ Updates global state "state" and tries different assignments of the next square. """
-#    print "-" * (y * width + x)
-#print "go(%d, %d)" % (y, x)
+    #print "go(%d, %d)" % (y, x)
+    #print print_status(y, x)
+    global best_red_dist, best_red_states, best_blue_dist, best_blue_states, state
     for ix in range(1, num_dist+1):
         state[y][x] = ix
-        status = check_state(state)
-        if status == STATE_ERROR:
+        #print "  trying %d" % ix
+        if check_state(state) == STATE_ERROR:
             # Error, undo it, try the next one
             #print "  ERROR"
             state[y][x] = 0
@@ -169,21 +239,77 @@ def go(y, x):
             go(y + 1, 0)
         else: # end of the grid, must be a valid state (already checked above)
             if check_state(state) == STATE_OK:
+                dist_red, dist_blue, dist_tie = evaluate_winners(state)
+#                if dist_red == best_red_dist:
+#                    print ""
+#                    print "%sAnother best red state (%d to %d)%s" % (RED, dist_red, dist_blue, CLEAR)
+#                    print_state(state)
+#                    best_red_states.append(copy.deepcopy(state))
+#                if dist_red > best_red_dist:
+#                    print ""
+#                    print "%sNew best red state (%d to %d)%s" % (RED, dist_red, dist_blue, CLEAR)
+#                    print_state(state)
+#                    best_red_dist = dist_red
+#                    best_red_states = [copy.deepcopy(state)]
+                if dist_blue == best_blue_dist:
+                    print ""
+                    print "%sAnother best blue state (%d to %d)%s" % (BLUE, dist_blue, dist_red, CLEAR)
+                    print_state(state)
+                    best_blue_states.append(copy.deepcopy(state))
+                if dist_blue > best_blue_dist:
+                    print ""
+                    print "%sNew best blue state (%d to %d)%s" % (BLUE, dist_blue, dist_red, CLEAR)
+                    print_state(state)
+                    best_blue_dist = dist_blue
+                    best_blue_states = [copy.deepcopy(state)]
+                
                 sys.stdout.write(".")
                 sys.stdout.flush()
-                valid_states.append(copy.deepcopy(state))
             else:
                 assert 0 # this can't happen, right?
 
-        # TODO I guess we undo the latest change and keep going?
+        # Undo the latest change and keep going
         state[y][x] = 0
 
 
-go(0,0)
+startx = 0
+starty = 0
+if False:
+    #pre-seed the first row of state
+    state[0][0] = 1
+    state[0][1] = 1
+    state[0][2] = 5
+    state[0][3] = 5
+    state[0][4] = 5
+    state[1][0] = 1
+    state[1][1] = 2
+    state[1][2] = 2
+    state[1][3] = 5
+    state[1][4] = 4
+    state[2][0] = 1
+    state[2][1] = 2
+    state[2][2] = 2
+    state[2][3] = 5
+    state[2][4] = 4
+    state[3][0] = 1
+    state[3][1] = 3
+    state[3][2] = 2
+    startx = 3
+    starty = 3
 
+go(starty, startx)
 
-for vs in valid_states:
-    print_state(vs)
+print ""
+print "=" * 80
+print "Final results:"
+print ""
+#print "%sBest red states (%d districts)%s" % (RED, best_red_dist, CLEAR)
+#for s in best_red_state:
+#    print ""
+#    print_state(s)
+print ""
+print "%sBest blue states (%d districts)%s" % (BLUE, best_blue_dist, CLEAR)
+for s in best_blue_state:
     print ""
-
+    print_state(s)
 
